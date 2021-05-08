f98b78560ce3e6eec1f01a48f6c3ba424d02db64
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-05-08 09:34:50 -0700

    Added files and method to find Kp

---
+++ findKp.m
@@ -0,0 +1,45 @@
+clc
+close all
+clear
+%%
+position = readtable("PositionSensorOrig.csv");
+timeOut = position.Time_s_;
+Vin = position.Channel2_V_;
+Vp = position.Math1_V_;
+
+plot(timeOut, Vp);
+
+zeros = [];
+VPlast = Vp(1);
+VPCur = Vp(2);
+for i = 2:size(Vp,1)
+    if VPlast > 0 && VPCur < 0
+        zeros = [zeros; timeOut(i), Vin(i), Vp(i)];
+    end
+    VPlast = Vp(i-1);
+    VPCur = Vp(i);
+end
+
+tRev = zeros(4,1) - zeros(2,1);
+
+%choosing 0.7 seconds as low end and 1.1 as high end
+
+denom = 2*pi - ((abs(zeros(2,1) - 0.7))+abs((zeros(4,1) - 1.1)));
+num = Vp(5201) - Vp(3601);
+
+Kp1 = num/denom;
+
+figure
+plot(timeOut(3601:5201), Vp(3601:5201));
+
+denom = 2*pi - (abs(zeros(2,1) - 0.5)+abs(zeros(1,1) - 0.1));
+num = Vp(2801) - Vp(1201);
+
+
+Kp2 = num/denom;
+
+figure
+plot(timeOut(1201:2801), Vp(1201:2801));
+
+
+Kpave = (Kp1+Kp2)/2
