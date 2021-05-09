4af91c0dbb946157da6ad233154ecadda2b1fe41
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-05-08 17:14:46 -0700

    Update findKp.m

--- findKp.m
+++ findKp.m
@@ -23,23 +23,39 @@ end
 tRev = zeros(4,1) - zeros(2,1);
 
 %choosing 0.7 seconds as low end and 1.1 as high end
-
+liveZone = 2 * pi * (1.1-0.7)/tRev;
 denom = 2*pi - ((abs(zeros(2,1) - 0.7))+abs((zeros(4,1) - 1.1)));
 num = Vp(5201) - Vp(3601);
 
+
 Kp1 = num/denom;
+Kp1p = num/liveZone;
 
 figure
 plot(timeOut(3601:5201), Vp(3601:5201));
 
+%choosing 0.1 seconds as low end and 0.5 as high end
+liveZone = 2 * pi * (0.5-0.1)/tRev;
 denom = 2*pi - (abs(zeros(2,1) - 0.5)+abs(zeros(1,1) - 0.1));
 num = Vp(2801) - Vp(1201);
 
 
 Kp2 = num/denom;
-
+Kp2p = num/liveZone;
 figure
 plot(timeOut(1201:2801), Vp(1201:2801));
 
 
-Kpave = (Kp1+Kp2)/2
+Kpave = (Kp1+Kp2)/2;
+Kpavep = (Kp1p+Kp2p)/2;
+
+figure
+hold on
+plot(timeOut, Vp);
+
+x = linspace(-10,10);
+y = Kp2 * x;
+
+line(x,y);
+
+hold off
