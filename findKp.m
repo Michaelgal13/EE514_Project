b2a042ba9c1878c65641cdd164f022082ebd3f76
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-05-12 09:34:58 -0700

    Graphed and found numerical values for taum

--- findKp.m
+++ findKp.m
@@ -67,7 +67,7 @@ line(x,y, 'Color', 'black', 'LineStyle', '--');
 xlabel('Radians');
 ylabel('Vp');
 legend('Data', 'Calculated Kp','Location', 'North West');
-im = [.2 .5 .3 .3];
+dim = [.2 .5 .3 .3];
 str = 'y = Kp*x-';
 annotation('textbox',dim,'String',str,'FitBoxToText','on');
 
@@ -110,6 +110,7 @@ dataA = [cycle.Time_s_(5150:5375), cycle.Math1_V_(5150:5375)]; % 3.8425 to 4.382
 dataB = [cycle.Time_s_(5550:5775), cycle.Math1_V_(5550:5775)]; % 4.84 to 5.405
 dataC = [cycle.Time_s_(5950:6175), cycle.Math1_V_(5950:6175)]; % 5.85 to 6.4
 dataD = [cycle.Time_s_(6350:6575), cycle.Math1_V_(6350:6575)]; % 6.84 to 7.365
+
 slop1 = (max(data1(:,2))- min(data1(:,2)))/(max(data1(:,1))- min(data1(:,1)));
 slop2 = (max(data2(:,2))- min(data2(:,2)))/(max(data2(:,1))- min(data2(:,1)));
 slop3 = (max(data3(:,2))- min(data3(:,2)))/(max(data3(:,1))- min(data3(:,1)));
@@ -232,6 +233,186 @@ legend("Stepwise", "Polyfit 1", "Polyfit 2", "Polyfit 3", "Polyfit 4", "Polyfit 
 
 hold off
 
+%%
+% find tau m
+checkConst = 20;
+
+movdat1 =[ data1(:,1) movmean(data1(:,2), 10)];
+movdat2 =[ data2(:,1) movmean(data2(:,2), 10)];
+movdat3 =[ data3(:,1) movmean(data3(:,2), 10)];
+movdat4 =[ data4(:,1) movmean(data4(:,2), 10)];
+movdat5 =[ data5(:,1) movmean(data5(:,2), 10)];
+movdat6 =[ data6(:,1) movmean(data6(:,2), 10)];
+movdat7 =[ data7(:,1) movmean(data7(:,2), 10)];
+movdat8 =[ data8(:,1) movmean(data8(:,2), 10)];
+movdat9 =[ data9(:,1) movmean(data9(:,2), 10)];
+movdatA =[ dataA(:,1) movmean(dataA(:,2), 10)];
+movdatB =[ dataB(:,1) movmean(dataB(:,2), 10)];
+movdatC =[ dataC(:,1) movmean(dataC(:,2), 10)];
+movdatD =[ dataD(:,1) movmean(dataD(:,2), 10)];
+
+tm1 = inf;
+for i = 1: size(movdat1,1) - checkConst
+    if (abs(movdat1(i + checkConst,2)- movdat1(i,2)))/(abs(movdat1(i + checkConst, 1)- movdat1(i,1))) >= (0.63 * slop1)
+        tm1 = abs(movdat1(i + checkConst, 1) - movdat1(1, 1));
+        break;
+    end
+end
+    
+tm2 = inf;
+for i = 1: size(movdat2,1) - checkConst
+    if (abs(movdat2(i + checkConst,2)- movdat2(i,2)))/(abs(movdat2(i + checkConst, 1)- movdat2(i,1))) >= (0.63 * slop1)
+        tm2 = abs(movdat2(i + checkConst, 1) - movdat2(1, 1));
+        break;
+    end
+end
+
+tm3 = inf;
+for i = 1: size(movdat3,1) - checkConst
+    if (abs(movdat3(i + checkConst,2)- movdat3(i,2)))/(abs(movdat3(i + checkConst, 1)- movdat3(i,1))) >= (0.63 * slop1)
+        tm3 = abs(movdat3(i + checkConst, 1) - movdat3(1, 1));
+        break;
+    end
+end
+
+tm4 = inf;
+for i = 1: size(movdat4,1) - checkConst
+    if (abs(movdat4(i + checkConst,2)- movdat4(i,2)))/(abs(movdat4(i + checkConst, 1)- movdat4(i,1))) >= (0.63 * slop1)
+        tm4 = abs(movdat4(i + checkConst, 1) - movdat4(1, 1));
+        break;
+    end
+end
+
+tm5 = inf;
+for i = 1: size(movdat5,1) - checkConst
+    if (abs(movdat5(i + checkConst,2)- movdat5(i,2)))/(abs(movdat5(i + checkConst, 1)- movdat5(i,1))) >= (0.63 * slop1)
+        tm5 = abs(movdat5(i + checkConst, 1) - movdat5(1, 1));
+        break;
+    end
+end
+
+tm6 = inf;
+for i = 1: size(movdat6,1) - checkConst
+    if (abs(movdat6(i + checkConst,2)- movdat6(i,2)))/(abs(movdat6(i + checkConst, 1)- movdat6(i,1))) >= (0.63 * slop1)
+        tm6 = abs(movdat6(i + checkConst, 1) - movdat6(1, 1));
+        break;
+    end
+end
+
+tm7 = inf;
+for i = 1: size(movdat7,1) - checkConst
+    if (abs(movdat7(i + checkConst,2)- movdat7(i,2)))/(abs(movdat7(i + checkConst, 1)- movdat7(i,1))) >= (0.63 * slop1)
+        tm7 = abs(movdat7(i + checkConst, 1) - movdat7(1, 1));
+        break;
+    end
+end
+
+tm8 = inf;
+for i = 1: size(movdat8,1) - checkConst
+    if (abs(movdat8(i + checkConst,2)- movdat8(i,2)))/(abs(movdat8(i + checkConst, 1)- movdat8(i,1))) >= (0.63 * slop1)
+        tm8 = abs(movdat8(i + checkConst, 1) - movdat8(1, 1));
+        break;
+    end
+end
+
+tm9 = inf;
+for i = 1: size(movdat9,1) - checkConst
+    if (abs(movdat9(i + checkConst,2)- movdat9(i,2)))/(abs(movdat9(i + checkConst, 1)- movdat9(i,1))) >= (0.63 * slop1)
+        tm9 = abs(movdat9(i + checkConst, 1) - movdat9(1, 1));
+        break;
+    end
+end
+
+tmA = inf;
+for i = 1: size(movdatA,1) - checkConst
+    if (abs(movdatA(i + checkConst,2)- movdatA(i,2)))/(abs(movdatA(i + checkConst, 1)- movdatA(i,1))) >= (0.63 * slop1)
+        tmA = abs(movdatA(i + checkConst, 1) - movdatA(1, 1));
+        break;
+    end
+end
+
+tmB = inf;
+for i = 1: size(movdatB,1) - checkConst
+    if (abs(movdatB(i + checkConst,2)- movdatB(i,2)))/(abs(movdatB(i + checkConst, 1)- movdatB(i,1))) >= (0.63 * slop1)
+        tmB = abs(movdatB(i + checkConst, 1) - movdatB(1, 1));
+        break;
+    end
+end
+
+
+tmC = inf;
+for i = 1: size(movdatC,1) - checkConst
+    if (abs(movdatC(i + checkConst,2)- movdatC(i,2)))/(abs(movdatC(i + checkConst, 1)- movdatC(i,1))) >= (0.63 * slop1)
+        tmC = abs(movdatC(i + checkConst, 1) - movdatC(1, 1));
+        break;
+    end
+end
+
+
+tmD = inf;
+for i = 1: size(movdatD,1) - checkConst
+    if (abs(movdatD(i + checkConst,2)- movdatD(i,2)))/(abs(movdatD(i + checkConst, 1)- movdatD(i,1))) >= (0.63 * slop1)
+        tmD = abs(movdatD(i + checkConst, 1) - movdatD(1, 1));
+        break;
+    end
+end
+
+horVals = 1:13;
+vertVals = [tm1, tm2, tm3, tm4, tm5, tm6, tm7, tm8, tm9, tmA, tmB, tmC, tmD];
+
+%plot(horVals,vertVals);
+
+average = mean(vertVals);
+
+totslop = sum(vertVals);
+vertNorm = vertVals./totslop;
+horNorm = 2*pi .* vertNorm;
+for i = size(horNorm,2) : -1: 1
+    horNorm(i) = sum(horNorm(1:i));
+end
+figure
+plot(horNorm, vertVals);
+title("tau m");
+figure
+hold on
+plot(horNorm, vertVals);
+%225 step time steps
+p1 = polyfit(horNorm, vertVals, 1);
+p2 = polyfit(horNorm, vertVals, 2);
+p3 = polyfit(horNorm, vertVals, 3);
+p4 = polyfit(horNorm, vertVals, 4);
+p5 = polyfit(horNorm, vertVals, 5);
+p6 = polyfit(horNorm, vertVals, 6);
+p7 = polyfit(horNorm, vertVals, 7);
+p8 = polyfit(horNorm, vertVals, 8);
+p9 = polyfit(horNorm, vertVals, 9);
+pA = polyfit(horNorm, vertVals, 10);
+
+y1 = polyval(p1, horNorm);
+y2 = polyval(p2, horNorm);
+y3 = polyval(p3, horNorm);
+y4 = polyval(p4, horNorm);
+y5 = polyval(p5, horNorm);
+y6 = polyval(p6, horNorm);
+y7 = polyval(p7, horNorm);
+y8 = polyval(p8, horNorm);
+y9 = polyval(p9, horNorm);
+yA = polyval(pA, horNorm);
 
 
 
+plot(horNorm,y1);
+plot(horNorm,y2);
+plot(horNorm,y3);
+plot(horNorm,y4);
+plot(horNorm,y5);
+plot(horNorm,y6);
+plot(horNorm,y7);
+plot(horNorm,y8);
+plot(horNorm,y9);
+plot(horNorm,yA);
+title("tau m");
+legend("Stepwise", "Polyfit 1", "Polyfit 2", "Polyfit 3", "Polyfit 4", "Polyfit 5", "Polyfit 6", "Polyfit 7", "Polyfit 8", "Polyfit 9", "Polyfit 10")
+
+
+hold off
