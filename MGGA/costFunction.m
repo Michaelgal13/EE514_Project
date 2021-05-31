4e2a08d97b8b13dc6035a364883d0214dedfc728
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-05-31 03:46:11 -0700

    Got GA initially working

--- MGGA/costFunction.m
+++ MGGA/costFunction.m
@@ -1,6 +1,11 @@
 function cost = costFunction(simOut)
 
 t = simOut.tout;
-y = simOut.yout;
+X = simOut.X;
+ctrleff = simOut.ctrl_eff;
+totCtrl = trapz(t,ctrleff);
+totX=trapz(t, X(:,1));
+totXdot = trapz(t,X(:,2));
+cost = abs(totCtrl + totX + totXdot);
 
-cost = y(1)^2;
+%cost = y(1)^2;
